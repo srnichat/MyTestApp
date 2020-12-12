69ef2276f313174dc8e1a7c8299969c5816d2a6b
srnichat <srnichat@gmail.com>
2020-12-12 13:24:09 +0530

    Changes made

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,10 +3,7 @@ public class HelloWorld{
     System.out.println("Hello World!!");
 	System.out.println("Hello World!!");
 	System.out.println("Hello World!!");
-	System.out.println("Hello World!!");
-	System.out.println("Hello World!!");
-	System.out.println("Hello World!!");
-	System.out.println("Hello World!!");
-	System.out.println("Hello World!!");
+	int a = 10;
+	System.out.println("a: "+a);
   }
 }
